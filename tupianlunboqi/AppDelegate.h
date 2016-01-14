7a31d8e78867b2b9a7c353517dde3dc27f34b51a
buruoyanyang <xu18716352164@gmail.com>
2016-01-14 23:25:28 +0800

    Initial Commit

---
+++ tupianlunboqi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tupianlunboqi
+//
+//  Created by xhf on 16/1/14.
+//  Copyright © 2016年 xhf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tupianlunboqi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tupianlunboqi
+//
+//  Created by xhf on 16/1/14.
+//  Copyright © 2016年 xhf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
