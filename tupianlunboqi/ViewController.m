2a5cb21d3e48df2558901902baf4d47f9e5e4dc9
buruoyanyang <xu18716352164@gmail.com>
2016-01-14 23:41:56 +0800

    2016/01/14
    家里 23：41

--- tupianlunboqi/ViewController.m
+++ tupianlunboqi/ViewController.m
@@ -9,14 +9,23 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    for (int i = 0; i < 5; i++) {
+        UIImageView *iconView = [[UIImageView alloc] init];
+        [self.scrollView addSubview:iconView];
+        NSString *imageName = [NSString stringWithFormat:@"img_%2d",i+1];
+        iconView.image = [UIImage imageNamed:imageName];
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning {
